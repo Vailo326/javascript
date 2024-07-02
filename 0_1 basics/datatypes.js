af89d8345c53ea2c0df6832afcb4f62f876836ef
Vailo326 <142527012+Vailo326@users.noreply.github.com>
2024-07-02 04:51:54 +0000

    datatypes

---
+++ 0_1 basics/datatypes.js
@@ -0,0 +1,12 @@
+// "use strict"
+
+let age=23 // 2 to power 53 // number
+let name="hitesh"            //"",'' two types of quotes
+//boolean=>true/false
+//null=>standalone value
+//symbol=>unique
+//undefined -->when the variable is declared but not initialized
+
+
+console.log(typeof undefined)// undefined
+console.log(typeof null) // object--> it means type of null is object in javascript
